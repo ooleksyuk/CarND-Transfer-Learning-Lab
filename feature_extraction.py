c30bb0a418c2181c8644979c1e8ef0c2909d1dd3
Olga Oleksyuk <ovoitovych@ezcater.com>
2017-07-03 13:38:44 -0400

    Add solution to py feature extraction file

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,13 +1,22 @@
 import pickle
 import tensorflow as tf
-# TODO: import Keras layers you need here
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
-flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
-flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+# CIFAR
+# flags.DEFINE_string('training_file', 'vgg-100/vgg_cifar10_100_bottleneck_features_train.p', "Bottleneck features training file (.p)")
+# flags.DEFINE_string('validation_file', 'vgg-100/vgg_cifar10_bottleneck_features_validation.p', "Bottleneck features validation file (.p)")
+# Traffis Sign
+flags.DEFINE_string('training_file', 'vgg-100/vgg_traffic_100_bottleneck_features_train.p', "Bottleneck features training file (.p)")
+flags.DEFINE_string('validation_file', 'vgg-100/vgg_traffic_bottleneck_features_validation.p', "Bottleneck features validation file (.p)")
+
+flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 256, "The batch size.")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -41,13 +50,20 @@ def main(_):
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
-    # TODO: define your model and hyperparams here
-    # make sure to adjust the number of classes based on
-    # the dataset
-    # 10 for cifar10
-    # 43 for traffic
+    np_classes = len(np.unique(y_train))
+    print("Found np_classes:", np_classes)
 
-    # TODO: train your model here
+    # define model
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(np_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # train model
+    # new version of keras is using nb_epochs instead of just `epochs`
+    model.fit(X_train, y_train, batch_size=FLAGS.batch_size, nb_epoch=FLAGS.epochs, validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
